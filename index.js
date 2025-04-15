8f6e25fc5c2a8d59519221381fea669e09ba024f
Linxedo <165984307+Linxedo@users.noreply.github.com>
2025-04-15 21:46:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-// Cyberpunk Style Animations
-document.querySelectorAll('a').forEach(anchor => {
-    anchor.addEventListener('mouseenter', () => {
-        anchor.style.transition = '0.2s ease-in-out';
-        anchor.style.transform = 'scale(1.1)';
-    });
+document.addEventListener("DOMContentLoaded", () => {
+    const navbarLinks = document.querySelectorAll('a');
 
-    anchor.addEventListener('mouseleave', () => {
-        anchor.style.transition = '0.2s ease-in-out';
-        anchor.style.transform = 'scale(1)';
-    });
-});
-
-// Scroll animation on clicking anchors
-document.querySelectorAll('a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-        
-        window.scrollTo({
-            top: targetElement ? targetElement.offsetTop - 20 : 0,
-            behavior: 'smooth'
+    navbarLinks.forEach(link => {
+        link.addEventListener('mouseover', () => {
+            link.style.transform = "scale(1.1)";
+        });
+        link.addEventListener('mouseout', () => {
+            link.style.transform = "scale(1)";
         });
     });
+
 });
