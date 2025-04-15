d141a107b1d88cec670edb9e7f76f65d7044ef19
Linxedo <165984307+Linxedo@users.noreply.github.com>
2025-04-15 20:18:10 +0800

    Delete blog.js

--- blog.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.article-content a').forEach(function(link) {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        alert('Artikel ini sedang dalam pengembangan.');
-    });
-});
