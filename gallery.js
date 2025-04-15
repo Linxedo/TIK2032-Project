ee9830b17776afe5e5923556a6b4be3111623f57
Linxedo <165984307+Linxedo@users.noreply.github.com>
2025-04-15 20:17:38 +0800

    Delete gallery.js

--- gallery.js
+++
@@ -1,8 +0,0 @@
-const images = document.querySelectorAll('.gallery img');
-
-images.forEach(function(img) {
-    img.addEventListener('click', function() {
-        const newWindow = window.open("", "_blank", "width=800,height=600");
-        newWindow.document.write("<img src='" + img.src + "' style='width:100%; height:auto;'>");
-    });
-});
