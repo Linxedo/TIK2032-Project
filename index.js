7ac2f3f988dec7d66390011fdc569619eecc38e2
Linxedo <165984307+Linxedo@users.noreply.github.com>
2025-04-15 20:17:21 +0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('a').forEach(function(link) {
-    link.addEventListener('click', function(event) {
-        alert('Anda akan diarahkan ke halaman ' + link.text);
-    });
-});
-
-window.onload = function() {
-    console.log("Halaman telah dimuat!");
-};
