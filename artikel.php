b9a38cb35d44f8932fab9067c1025f5531dc30b9
Linxedo <165984307+Linxedo@users.noreply.github.com>
2025-05-21 15:03:54 +0800

    Update artikel.php

--- artikel.php
+++ artikel.php
@@ -1,30 +1,23 @@
 <?php
-// Aktifkan error reporting
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-// Pastikan tidak ada output sebelum header
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 
-// Konfigurasi database
 $host = "localhost";
 $user = "root";
 $password = "";
 $dbname = "website_db";
 
-// Membuat koneksi
 $conn = new mysqli($host, $user, $password, $dbname);
 
-// Cek koneksi
 if ($conn->connect_error) {
     http_response_code(500);
     echo json_encode(["error" => "Koneksi database gagal: " . $conn->connect_error]);
     exit;
 }
 
-// Query untuk mengambil data artikel
 $sql = "SELECT id, judul, isi, tanggal, gambar FROM artikel ORDER BY tanggal DESC";
 $result = $conn->query($sql);
 
@@ -43,7 +36,6 @@ if ($result->num_rows > 0) {
     }
     echo json_encode($artikel);
 } else {
-    // Tidak ada data, kembalikan array kosong
     echo json_encode(["message" => "Tidak ada artikel ditemukan", "data" => []]);
 }
 
